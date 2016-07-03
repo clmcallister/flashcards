8823d89760bca124f34f3feab67c1afb04671478
clmcallister <lamont152@gmail.com>
2016-07-03 12:13:24 -0400

    Add toString, equals, and hashCode methods to model classes.

--- flashcards/src/main/java/net/charliemcallister/flashcards/model/FlashCard.java
+++ flashcards/src/main/java/net/charliemcallister/flashcards/model/FlashCard.java
@@ -62,4 +62,51 @@ public class FlashCard implements Serializable {
 	public void setTopic(Topic topic) {
 		this.topic = topic;
 	}
+	
+	@Override
+	public String toString() {
+		return "FlashCard [id=" + id + ", question=" + question + ", answer="
+				+ answer + ", topic=" + topic + "]";
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((answer == null) ? 0 : answer.hashCode());
+		result = prime * result + id;
+		result = prime * result
+				+ ((question == null) ? 0 : question.hashCode());
+		result = prime * result + ((topic == null) ? 0 : topic.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FlashCard other = (FlashCard) obj;
+		if (answer == null) {
+			if (other.answer != null)
+				return false;
+		} else if (!answer.equals(other.answer))
+			return false;
+		if (id != other.id)
+			return false;
+		if (question == null) {
+			if (other.question != null)
+				return false;
+		} else if (!question.equals(other.question))
+			return false;
+		if (topic == null) {
+			if (other.topic != null)
+				return false;
+		} else if (!topic.equals(other.topic))
+			return false;
+		return true;
+	}
 }
